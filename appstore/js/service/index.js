cd1f1155a346556c4e0a80940c5833c2426e442b
abjia <daohao1914>
2014-09-17 14:57:52 +0800

    提交切换代码

--- appstore/js/service/index.js
+++ appstore/js/service/index.js
@@ -1,35 +1,45 @@
+mui.init({
+	swipeBack: false,
+	//预加载
+	preloadPages: [{
+		url: "/page/category.html",
+		id: "category"
+	}, {
+		url: "/page/pay.html",
+		id: "pay"
+	}],
+	subpages: [{
+		id: "list",
+		url: "list.html",
+		styles: {
+			top: "48px",
+			bottom: 0
+		}
+	}]
+});
+
+
 var UI = {
 	cateGoryBnt: document.querySelector("#categoryBtn"),
-	preorder   : document.querySelector("#preOrderBtn")
+	preorder: document.querySelector("#preOrderBtn")
 };
 
 //弹出类别新窗口
 UI.cateGoryBnt.addEventListener("tap", function() {
-	console.log("ccc");
-	$.createWindow({
-      url: "/page/category.html",
-      preload: true//必需
-    });
-    
-    
-//	mui.openWindow({
-//		url: '/page/category.html',
-//		id: 'category',
-//		show: {
-//			autoShow: true, //页面loaded事件发生后自动显示，默认为true
-//			aniShow: "slide-in-bottom", //页面显示动画，默认为”slide-in-right“；
-//		}
-//	});
+	mui.openWindow({
+		id: 'category',
+		show: {
+			aniShow: "slide-in-bottom", //页面显示动画，默认为”slide-in-right“；
+		}
+	});
 });
 
 
 //弹出预购清单新窗口
 UI.preorder.addEventListener("tap", function() {
 	mui.openWindow({
-		url: '/page/category.html',
-		id: 'category',
+		id: 'pay',
 		show: {
-			autoShow: true, //页面loaded事件发生后自动显示，默认为true
 			aniShow: "slide-in-bottom", //页面显示动画，默认为”slide-in-right“；
 		}
 	});
